c2ce0eeb95a7576403452e6f71b8d2744fd93c7f
localworkarea <localworkarea@gmail.com>
2025-04-15 21:22:51 +0300

    up

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -4,19 +4,23 @@ import { isMobile } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 window.addEventListener("load", () => {
-
+  const sectionOne = document.querySelector('.section-0');
   document.documentElement.classList.remove("_step-01", "_step-02", "_step-03");
 
   setTimeout(() => {
     document.documentElement.classList.add("_step-01");
-
+    sectionOne.style.pointerEvents = "none";
     setTimeout(() => {
       document.documentElement.classList.add("_step-02");
 
       setTimeout(() => {
         document.documentElement.classList.add("_step-03");
 
-      }, 1200);
+        setTimeout(() => {
+          sectionOne.style.pointerEvents = "all";
+        }, 2500);
+      
+      }, 600);
 
     }, 3500);
 
