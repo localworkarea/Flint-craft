530b0af352b4a0683c0bb4fb7e7e0174412b5e80
localworkarea <localworkarea@gmail.com>
2025-04-15 16:37:13 +0300

    up

--- docs/js/app.js
+++ docs/js/app.js
@@ -348,6 +348,18 @@
             }));
         }
     }), 0);
+    window.addEventListener("load", (() => {
+        document.documentElement.classList.remove("_step-01", "_step-02", "_step-03");
+        setTimeout((() => {
+            document.documentElement.classList.add("_step-01");
+            setTimeout((() => {
+                document.documentElement.classList.replace("_step-01", "_step-02");
+                setTimeout((() => {
+                    document.documentElement.classList.replace("_step-02", "_step-03");
+                }), 1e3);
+            }), 2500);
+        }), 0);
+    }));
     window["FLS"] = false;
     addLoadedClass();
 })();
--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,23 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+window.addEventListener("load", () => {
+
+  document.documentElement.classList.remove("_step-01", "_step-02", "_step-03");
+
+  setTimeout(() => {
+    document.documentElement.classList.add("_step-01");
+
+    setTimeout(() => {
+      document.documentElement.classList.replace("_step-01", "_step-02");
+
+      setTimeout(() => {
+        document.documentElement.classList.replace("_step-02", "_step-03");
+
+      }, 1000);
+
+    }, 2500);
+
+  }, 0);
+});
